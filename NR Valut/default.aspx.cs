0acecbdafade58eef95b0a18943e0655dad4e0d3
Shayshu-NR <shayshu.nahata@mail.utoronto.ca>
2021-05-27 21:36:04 -0400

    Switching to .net framework

---
+++ NR Valut/default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NR_Valut
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        public string Login(string username, string password){
+            
+        }
+    }
+}
